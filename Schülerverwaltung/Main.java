bf9c2465901f4f9bbe59933f8ffd8d33a966de70
Simon Kebekus <56955651+Foxel05@users.noreply.github.com>
2022-12-06 11:20:59 +0100

    mergeSort

--- Schülerverwaltung/Main.java
+++ Schülerverwaltung/Main.java
@@ -232,8 +232,65 @@ public class Main
         
     }
 
-    public static void mergeSort(){
+    public static void mergeSort(List<Schueler> givenList){
+        if (givenList.getLength() == 1) return; //Wenn die Liste nur einen Shüler beinhaltet, zurück (da man schlecht nur eine Sache sortieren kann)
+
+        givenList.toFirst();
         
+        int i = 0;
+        List<Schueler> ersteListe = new List<Schueler>();
+        List<Schueler> zweiteListe = new List<Schueler>();
+
+        while (i < (givenList.getLength() / 2)){
+            i++;
+            ersteListe.append(givenList.getContent());
+            givenList.next();
+        }
+        while (givenList.hasAccess()){
+            zweiteListe.append(givenList.getContent());
+            givenList.next();
+        }
+
+        if (ersteListe.getLength() > 1){
+            ersteListe = mergeSort(ersteListe);
+        }
+        else if (zweiteListe.getLength() > 1){
+            zweiteListe = mergeSort(zweiteListe);
+        }
+
+        ersteListe.toFirst();
+        zweiteListe.toFirst();
+
+        List<Schueler> newList = new List<Schueler>();
+        
+        while (!ersteListe.isEmpty() && !zweiteListe.isEmpty()){
+            if (ersteListe.isEmpty()){
+                newList.append(zweiteListe.getContent());
+                zweiteListe.remove();
+            }
+            else if (zweiteListe.isEmpty()){
+                newList.append(ersteListe.getContent());
+                ersteListe.remove();
+            }
+            else if (ersteListe.getContent().getSprung() == zweiteListe.getContent().getSprung()){
+                newList.append(ersteListe.getContent());
+                ersteListe.remove();
+                newList.append(zweiteListe.getContent());
+                zweiteListe.remove();
+            }
+            else if (ersteListe.getContent().getSprung() > zweiteListe.getContent().getSprung()){
+                newList.append(zweiteListe.getContent());
+                zweiteListe.remove();
+            }
+            else if (ersteListe.getContent().getSprung() < zweiteListe.getContent().getSprung()){
+                newList.append(ersteListe.getContent());
+                ersteListe.remove();
+            } 
+        }
+        return newList;
+
+
+        }
     }
 //weitere Dienste, die einiges vereinfachen!!!
     public static void selectStudentend(Schueler x){
