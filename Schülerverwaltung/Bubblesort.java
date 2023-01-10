d7af6bd12bf38e2f04ebca878f0573307eeab384
Simon Kebekus <56955651+Foxel05@users.noreply.github.com>
2023-01-10 13:03:05 +0100

    Sheesh Bubblesort

---
+++ Schülerverwaltung/Bubblesort.java
@@ -0,0 +1,29 @@
+public class Bubblesort {
+
+    public Bubblesort() {
+
+    }
+
+    public static List<Schueler> sort(List<Schueler> givenList) {
+        List<Schueler> sortedList = new List<Schueler>();
+        Schueler copyedschueler = null;
+        int i = givenList.length - 1;
+        while (i != 0){
+            givenList.toFirst();
+            while (givenList.hasAccess()){
+                copyedschueler = givenList.getContent();
+                givenList.remove();
+                if (givenList.getContent.getSprung() > copyedschueler.getSprung()){
+                    givenList.insert(copyedschueler);
+                }
+                else if (givenList.getContent.getSprung() < copyedschueler.getSprung()){
+                    givenList.insert(givenList.getContent());
+                    givenList.setContent(copyedschueler);
+                }
+            }
+            i = i - 1;
+        }
+        return givenList;
+    }
+
+}
